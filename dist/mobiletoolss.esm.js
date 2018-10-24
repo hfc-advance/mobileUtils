e3f1cc4df30358a907ab7d0d2ea02de038aa3249
崔海峰 <cuihaifeng1919@hotmail.com>
2018-10-24 15:25:04 +0800

    add readme

--- dist/mobiletoolss.esm.js
+++ dist/mobiletoolss.esm.js
@@ -1,29 +1,34 @@
 /*!
- * mobiletoolss.js v1.0.2
+ * mobiletoolss.js v1.0.3
  * (c) 2018-2018 崔海峰
  * Released under the MIT License.
  */
 //! 判断浏览器是否支持scroll-behavior
 function isSupportSmoothScroll() {
-  let isSupport = !!window.getComputedStyle(document.body).scrollBehavior;
-  isSupportSmoothScroll = () => isSupport;
+  var isSupport = !!window.getComputedStyle(document.body).scrollBehavior;
+  isSupportSmoothScroll = function isSupportSmoothScroll() {
+    return isSupport;
+  };
   return isSupport;
 }
 
 //! 必须传递参数
 function mustParam(param) {
-  throw new Error(`${param} are required`);
+  throw new Error(param + " are required");
 }
 
 //! 判断两个元素之间的距离元素的顶部，以及元素的最左边
-function EleDistanceEle(childEle = mustParam('childEle'), parentEle = mustParam('parentEle')) {
-  if (!(childEle instanceof Element)) throw new Error(`childEle must be Element`);
-  if (!(parentEle instanceof Element)) throw new Error(`parentEle must be Element`);
+function EleDistanceEle() {
+  var childEle = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : mustParam('childEle');
+  var parentEle = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : mustParam('parentEle');
+
+  if (!(childEle instanceof Element)) throw new Error('childEle must be Element');
+  if (!(parentEle instanceof Element)) throw new Error('parentEle must be Element');
   //! 最大遍历次数(也就是DOM最大嵌套层级)
-  let mustWhileExeCount = 30;
-  let initExeCount = 0;
-  let isSuccess = true;
-  let result = {
+  var mustWhileExeCount = 30;
+  var initExeCount = 0;
+  var isSuccess = true;
+  var result = {
     top: 0,
     left: 0
   };
@@ -39,9 +44,12 @@ function EleDistanceEle(childEle = mustParam('childEle'), parentEle = mustParam(
 }
 
 //! 带有缓动效果的滚动
-function easeVerticalScroll(target = mustParam('target'), scrollDOM = mustParam('scrollDOM')) {
-  if (!(target instanceof Element)) throw new Error(`target must be Element`);
-  if (!(scrollDOM instanceof Element)) throw new Error(`scrollDOM must be Element`);
+function easeVerticalScroll() {
+  var target = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : mustParam('target');
+  var scrollDOM = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : mustParam('scrollDOM');
+
+  if (!(target instanceof Element)) throw new Error('target must be Element');
+  if (!(scrollDOM instanceof Element)) throw new Error('scrollDOM must be Element');
   //! 原生支持
   if (isSupportSmoothScroll()) {
     target.scrollIntoView({
@@ -49,43 +57,47 @@ function easeVerticalScroll(target = mustParam('target'), scrollDOM = mustParam(
       block: 'start'
     });
   } else {
-    let distance = EleDistanceEle(target, scrollDOM);
+    var distance = EleDistanceEle(target, scrollDOM);
     //! TODO: 避免出问题，设置个最大滚动次数
-    let scrollCount = 30;
-    let initScrollCount = 1;
+    var scrollCount = 30;
+    var initScrollCount = 1;
     //! 是否是全局滚动
-    let isBody = scrollDOM.isEqualNode(document.body);
+    var isBody = scrollDOM.isEqualNode(document.body);
     //! 滚动元素的最大滚动距离
-    let maxScroll = isBody ? scrollDOM : scrollDOM.scrollHeight - (scrollDOM.offsetHeight || 0);
+    var maxScroll = isBody ? scrollDOM : scrollDOM.scrollHeight - (scrollDOM.offsetHeight || 0);
     //! 目标距离
-    let targetPlace = distance.top > maxScroll ? maxScroll : distance.top;
-    let getScrollTop = () => {
+    var targetPlace = distance.top > maxScroll ? maxScroll : distance.top;
+    var _getScrollTop = function getScrollTop() {
       if (!isBody) {
-        getScrollTop = () => {
+        _getScrollTop = function getScrollTop() {
           return scrollDOM.scrollTop || 0;
         };
         return scrollDOM.scrollTop || 0;
       } else {
-        getScrollTop = () => {
+        _getScrollTop = function getScrollTop() {
           return window.pageYOffset || 0;
         };
         return window.pageYOffset || 0;
       }
     };
     //! 设置顶部的距离
-    let setScrollTop = (number = 0) => {
+    var _setScrollTop = function setScrollTop() {
+      var number = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+
       if (!isBody) {
-        setScrollTop = number3 => {
+        _setScrollTop = function setScrollTop(number3) {
           scrollDOM.scrollTop = number3;
         };
         scrollDOM.scrollTop = number;
       } else {
         if (document.body && document.body.scrollTop) {
-          setScrollTop = (number1 = 0) => {
+          _setScrollTop = function setScrollTop() {
+            var number1 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+
             document.body.scrollTop = number1;
           };
         } else if (document.documentElement && document.documentElement.scrollTop) {
-          setScrollTop = number2 => {
+          _setScrollTop = function setScrollTop(number2) {
             document.documentElement.scrollTop = number2;
           };
         }
@@ -93,23 +105,23 @@ function easeVerticalScroll(target = mustParam('target'), scrollDOM = mustParam(
       }
     };
     //! 处理兼容性
-    let requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || function (callback) {
+    var requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || function (callback) {
       return setTimeout(callback, 1000 / 60);
     };
-    let cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.clearTimeout;
-    let timer = requestAnimationFrame(function fn() {
+    var cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.clearTimeout;
+    var timer = requestAnimationFrame(function fn() {
       //! 当前相差多少
-      let currentDistance = parseInt(targetPlace) - getScrollTop();
+      var currentDistance = parseInt(targetPlace) - _getScrollTop();
       if (Math.abs(currentDistance) <= 1 || initScrollCount >= scrollCount) {
-        setScrollTop(targetPlace);
+        _setScrollTop(targetPlace);
         cancelAnimationFrame(timer);
         initScrollCount = 1;
       } else {
         initScrollCount += 1;
-        let shouldScroll = currentDistance * 0.25;
+        var shouldScroll = currentDistance * 0.25;
         //! TODO:小于1的时候scrollTop设置了不会触发滚动
-        let shouldScrollTop = getScrollTop() + (shouldScroll > 0 ? shouldScroll < 1 ? 1 : shouldScroll : shouldScroll > -1 ? -1 : shouldScroll);
-        setScrollTop(shouldScrollTop);
+        var shouldScrollTop = _getScrollTop() + (shouldScroll > 0 ? shouldScroll < 1 ? 1 : shouldScroll : shouldScroll > -1 ? -1 : shouldScroll);
+        _setScrollTop(shouldScrollTop);
         timer = requestAnimationFrame(fn);
       }
     });
@@ -118,9 +130,9 @@ function easeVerticalScroll(target = mustParam('target'), scrollDOM = mustParam(
 
 var main = {
   //! 平滑滚动
-  easeVerticalScroll,
+  easeVerticalScroll: easeVerticalScroll,
   //! 计算两个元素之间的距离
-  EleDistanceEle
+  EleDistanceEle: EleDistanceEle
 };
 
 export default main;
